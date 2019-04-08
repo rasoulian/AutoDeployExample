f01974dcc40ff86df6142f10a44ee1ebb41248e6
rasoulian <arasoulian@gmail.com>
2019-04-08 09:03:58 +0430

    autodeploy done

--- set_tags.sh
+++ set_tags.sh
@@ -5,8 +5,8 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
   	if [ -z "$TRAVIS_TAG" ]; then
 	    echo -e "Starting to tag commit.\n"
 
-	    git config --global user.email "travis@travis-ci.org"
-	    git config --global user.name "Travis"
+	    git config --global user.email "arasoulian@gmail.com"
+	    git config --global user.name "rasoulian"
 
 	    # Add tag and push to master.
 	    git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
